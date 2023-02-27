1ec7ea7ebbf85c8e0fbe31fccb24d5686d3e2d6a
chukwudibarrah <ceebrrh@gmail.com>
2023-02-27 11:06:46 +0000

    Added questions structure and inquirer prompt to start questions

--- index.js
+++ index.js
@@ -2,70 +2,156 @@ import fs from "fs";
 import path from "path";
 import inquirer from "inquirer";
 import generateMarkdown from "./starter/utils/generateMarkdown.js";
+import util from "util";
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-      },
-      {
-        type: "input",
-        name: "motivation",
-        message: "What was your motivation for creating this project?",
-      },
-      {
-        type: "input",
-        name: "function",
-        message: "What does the application do?",
-      },
-      {
-        type: "input",
-        name: "solution",
-        message: "What problem does it solve?",
-      },
-      {
-        type: "input",
-        name: "lessons",
-        message: "What did you learn?",
-      },
-      {
-        type: "input",
-        name: "steps",
-        message:
-          "What are the steps required to install your project? (Step-by-step instruction)",
-      },
-      {
-        type: "input",
-        name: "collaboration",
-        message: "Who did you collaborate with?",
-      },
-      {
-        type: "input",
-        name: "collaboration",
-        message:
-          "Include SimpleIcon logos? (Copy and paste your preferred logo(s) from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)",
-      },
-      {
-        type: "input",
-        name: "features",
-        message: "List the features of your project (separated by commas)",
-      },
-      {
-        type: "input",
-        name: "contributions",
-        message: "How can developers contribute to your project?",
-      },
-    ])
-    .then((response) => console.log(response));
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "motivation",
+      message: "What was your motivation for creating this project?",
+    },
+    {
+      type: "input",
+      name: "function",
+      message: "What does the application do?",
+    },
+    {
+      type: "input",
+      name: "solution",
+      message: "What problem does it solve?",
+    },
+    {
+      type: "input",
+      name: "lessons",
+      message: "What did you learn?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "How is the application used? (include instructions and examples for use)",
+    },
+    {
+      type: "input",
+      name: "steps",
+      message:
+        "What are the steps required to install your project? (Step-by-step instruction)",
+    },
+    {
+      type: "input",
+      name: "collaboration",
+      message:
+        "Who did you collaborate with? (include any third party assets and or tutorials that helped along the way)",
+    },
+    {
+      type: "input",
+      name: "badges",
+      message:
+        "Include SimpleIcon logos? (Copy and paste your preferred logo(s) from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "List the features of your project (separated by commas)",
+    },
+    {
+      type: "input",
+      name: "contributions",
+      message: "How can developers contribute to your project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "What are some tests of your application and how can users to run them?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+  ]);
+// .then((answers) => console.log(response));
 
-// function to write README file
-function writeToFile(fileName, data) {}
+const generateMd = (answers) =>
+  `# ${answers.title}
 
-// function to initialize program
-function init() {}
+## Description
 
-// function call to initialize program
-init();
+${answers.motivation}
+${answers.function}
+${answers.solution}
+${answers.lessons}
+
+## Table of Contents
+
+Description
+Table of Contents
+Installation
+Usage
+Credits
+License
+How o contribute
+Tests
+Questions
+
+## Installation
+
+${answers.steps}
+
+## Usage
+
+${answers.usage}
+
+## Credits
+
+${answers.collaboration}
+
+## Badges
+
+${answers.badges}
+
+## Features
+
+${answers.features}
+
+## How to Contribute
+
+${answers.contributions}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+${answers.github}
+${answers.email}`;
+
+promptUser()
+  .then((answers) => writeFileAsync("README.md", generateMd(answers)))
+  .then(() => console.log("Successfully wrote to README.md"))
+  .catch((err) => console.error(err));
+
+// // function to write README file
+// function writeToFile(`${answers.title}`, answers) {}
+
+// // function to initialize program
+// function init() {}
+
+// // function call to initialize program
+// init();
